89cadffcdabf94f2d2b42b9bbb54ca36bdeade52
Uwe Hale <uhale@vmware.com>
2016-09-19 19:57:26 -0500

    Cleaned up error message
    Change-Id: I047acbf974754607b794b0fcaeca07323570a0e9

--- approval-service/server/models/approval/ApprovalRequestException.js
+++ approval-service/server/models/approval/ApprovalRequestException.js
@@ -1,14 +1,6 @@
 var approvalRequestException = function (field) {
     this.name = 'ApprovalRequestException';
-    this.message = "Invalid Approval Request Fields:" + field +
-
-    "\n\
-    { \n\
-        \"name\": \"ServerName\", \n\
-        \"date\": \"YYYYMMDD\", \n\
-        \"user\": \"First Last\", \n\
-        \"email\": \"flast@vmware.com\" \n\
-    }\";\n";
+    this.message = 'Invalid Approval Request Fields:' + field ;
 
 
 };
