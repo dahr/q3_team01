725d25389572633bb1e4a22859d69bcda5d58f03
ellisd <ellisd@vmware.com>
2016-10-18 10:12:30 -0500

    Correct spelling of "Function"

--- testing-service/runtests.sh
+++ testing-service/runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /opt/SoapUI/bin/testrunner.sh \
--s"Basic Fuction Suite" \
+-s"Basic Function Suite" \
 -r -a \
 -f/home/app/reports/ \
 /home/app/Q3-Training-Tests-soapui-project.xml
