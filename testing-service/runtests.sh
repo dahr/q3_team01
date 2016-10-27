c388bc282a39e2b6bd6b3cc3a97e086a74f774d3
Smalling, Eric A <esmalling@vmware.com>
2016-10-27 00:44:10 -0500

    pointing test runner at Photon

--- testing-service/runtests.sh
+++ testing-service/runtests.sh
@@ -5,7 +5,7 @@
 project_dir="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 
 
-#curl --form "project=@${project_dir}/Q3-Training-Tests-soapui-project.xml"  --form "suite=Basic Function Suite" http://192.168.110.89:3000
+curl --form "project=@${project_dir}/Q3-Training-Tests-soapui-project.xml"  --form "suite=Basic Function Suite" http://192.168.110.89:3000
 #curl --form "project=@${project_dir}/Q3-Training-Tests-soapui-project.xml"  --form "suite=Basic Function Suite" http://app.docna-team1.io:3000
 #curl --form "project=@${project_dir}/Q3-Training-Tests-soapui-project.xml"  --form "suite=Basic Function Suite" http://172.16.78.129:3000
-curl --form "project=@${project_dir}/Q3-Training-Tests-soapui-project.xml"  --form "suite=Basic Function Suite" http://localhost:3000
+#curl --form "project=@${project_dir}/Q3-Training-Tests-soapui-project.xml"  --form "suite=Basic Function Suite" http://localhost:3000
